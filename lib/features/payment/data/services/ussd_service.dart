ad7e14a102d8886948813eefd7a56a75a63639ac
Priyankesh <priyankeshom@gmail.com>
2026-02-19 23:11:47 +0530

    feat: implement runtime phone permission check before dialing

--- lib/features/payment/data/services/ussd_service.dart
+++ lib/features/payment/data/services/ussd_service.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class UssdService {
   static const MethodChannel _channel = MethodChannel('com.chaltapay/ussd');
   static const EventChannel _eventChannel = EventChannel('com.chaltapay/ussd_events');
 
   Future<void> dialUssd(String code) async {
+    final status = await Permission.phone.status;
+    if (!status.isGranted) {
+      final result = await Permission.phone.request();
+      if (!result.isGranted) {
+        throw Exception('Phone permission is required to dial USSD codes.');
+      }
+    }
+
     try {
       await _channel.invokeMethod('dial', {'code': code});
     } on PlatformException catch (e) {
